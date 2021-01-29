50b6f9ee6efc5bc2d9ff57efcd295255637be15d
Luc-Leveque <31991589+Luc-Leveque@users.noreply.github.com>
2021-01-29 17:46:47 +0100

    t4

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -4,14 +4,10 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 let apolloClient
 
 function createApolloClient() {
-  console.log(
-    '🚀 ~ file: apolloClient.js ~ line 12 ~ createApolloClient ~ process.env.API_URL',
-    process.env.API_URL
-  )
   return new ApolloClient({
     ssrMode: typeof window === 'undefined', // set to true for SSR
     link: new HttpLink({
-      uri: process.env.API_URL | 'http://localhost:8181/graphql'
+      uri: process.env.API_URL || 'http://localhost:8181/graphql'
     }),
     cache: new InMemoryCache()
   })
